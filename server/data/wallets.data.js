3d08082d8faf482e9fc2f1658eceb11fa7c54ede
Luis Fernando <guffenix@gmail.com>
2023-07-05 18:54:51 -0500

    fix: export data

--- server/data/wallets.data.js
+++ server/data/wallets.data.js
@@ -1,5 +1,9 @@
-export const balances = new Map([
+const balances = new Map([
   ['02f2abb44e4007d3c4bee785c6bcd5734632e0f2ee867d6e22b84e00a3fa49eb75', 100],
   ['03ebf37e0adb62505ceb4bc9e38a16367392db2df28bf4aa98d84864ce835bf07d', 50],
   ['03d42c89f68e90a859b008163411de1c8a87dbd656e75ddd4743f049ab0b88dbe3', 75],
 ])
+
+module.exports = {
+  balances,
+}
