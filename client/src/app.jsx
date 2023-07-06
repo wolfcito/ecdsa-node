27c80f94c9a711f9bbd25b25afd4ddb9859a55c1
Luis Fernando <guffenix@gmail.com>
2023-07-05 19:21:51 -0500

    chore: improve file structure

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
 import './App.scss'
-import { Transfer } from '~/components/transfer'
-import { Wallet } from '~/components/wallet'
+import { Transfer } from '~/components/transfer/transfer.component'
+import { Wallet } from '~/components/wallet/wallet.component'
 
 export default function App() {
   const [balance, setBalance] = useState(0)
