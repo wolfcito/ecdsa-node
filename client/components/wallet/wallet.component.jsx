7d276041001ac960e6f2e191a219a393c2ab3427
Luis Fernando <guffenix@gmail.com>
2023-07-05 23:00:03 -0500

    chore: improve ui for wallet selection

--- client/components/wallet/wallet.component.jsx
+++ client/components/wallet/wallet.component.jsx
@@ -20,22 +20,29 @@ export function Wallet({ balance, setBalance, user, setUser }) {
   }
 
   return (
-    <div className="wallet container">
-      <h1>Select your wallet</h1>
+    <section className="mt-10 flex flex-col">
+      <h1 className="font-semibold sm:mx-auto sm:w-full sm:max-w-sm">
+        Wallet funds
+      </h1>
+      <div className="flex flex-col gap-5 rounded-lg border-2 border-dashed border-gray-300 p-5 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="flex items-center justify-between">
+          Sender Address
+          <select
+            onChange={onSelectUser}
+            value={user}
+            className="mt-2 block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          >
+            <option value="">Choose a user wallet </option>
+            {USERS.map((u, i) => (
+              <option key={nanoid()} value={u}>
+                {u}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <label>
-        Wallet Address
-        <select onChange={onSelectUser} value={user} className="balance">
-          <option value="">--- please choose a user wallet ---</option>
-          {USERS.map((u, i) => (
-            <option key={nanoid()} value={u}>
-              {u}
-            </option>
-          ))}
-        </select>
-      </label>
-
-      <div className="balance rounded-sm">Balance: {balance}</div>
-    </div>
+        <div className="balance rounded-sm">Balance: {balance}</div>
+      </div>
+    </section>
   )
 }
