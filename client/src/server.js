f4473fa3d636770403d97b24e15ca864183367d9
Luis Fernando <guffenix@gmail.com>
2023-07-06 19:16:32 -0500

    build: deploy config

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+//http://localhost:3042'
+
 const server = axios.create({
-  baseURL: 'http://localhost:3042',
+  baseURL: 'https://ecdsa-server-wolfcito.vercel.app/',
 })
 
 export default server
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -11,7 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '~/': './',
+      '~': path.resolve(__dirname, './'),
     },
   },
 })
